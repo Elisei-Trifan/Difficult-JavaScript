fc97925fa9fccba72e2e37ea1e0062465b7b6b97
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-09 16:38:56 +0300

    add callback function

--- 4.1-promises-practic.js
+++ 4.1-promises-practic.js
@@ -6,6 +6,22 @@ function fetchUserInfo(callback) {
   }, 100)
 }
 
+function fetchUserGames(id, callback) {
+  setTimeout(() => {
+    /// fetch(id)
+    const games = ['game1', 'game2']
+    callback(games)
+  }, 2000)
+}
+
+function fetchUserCars(id, callback) {
+  ///fetch
+  setTimeout(() => {
+    const cars = ['mers', 'bmw', 'opel']
+    callback(cars)
+  }, 3000)
+}
+
 function run() {
   fetchUserInfo((userInfo) => {
     console.log(userInfo)
